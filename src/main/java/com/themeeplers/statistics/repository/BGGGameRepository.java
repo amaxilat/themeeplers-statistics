d9e26189ce1e6c358d7edd5f48be34edc3b5872f
amaxilat <d.amaxilatis@sparkworks.net>
2018-03-30 01:15:40 +0300

    revert to selecting the weight and rating

--- src/main/java/com/themeeplers/statistics/repository/BGGGameRepository.java
+++ src/main/java/com/themeeplers/statistics/repository/BGGGameRepository.java
@@ -3,6 +3,7 @@ package com.themeeplers.statistics.repository;
 import com.themeeplers.statistics.model.db.BGGGame;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +11,9 @@ import org.springframework.stereotype.Repository;
 public interface BGGGameRepository extends PagingAndSortingRepository<BGGGame, Long> {
     BGGGame findByBggId(Long bggId);
 
-    @Query("SELECT b from bgg_game where b.weight=(SELECT max(a.weight) FROM bgg_game a) limit 1")
-    BGGGame findTopOrderByWeightDesc();
+    @Query("SELECT max(a.weight) FROM bgg_game a")
+    Double findTopOrderByWeightDesc();
 
-    @Query("SELECT b from bgg_game where b.rating=(SELECT max(a.rating) FROM bgg_game a) limit 1")
-    BGGGame findTopOrderByRatingDesc();
+    @Query("SELECT max(a.rating) FROM bgg_game a")
+    Double findTopOrderByRatingDesc();
 }
