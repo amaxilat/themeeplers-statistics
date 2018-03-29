5a6da967f911461ad3a9315cc5459f99f2573207
amaxilat <d.amaxilatis@sparkworks.net>
2018-03-30 01:05:49 +0300

    use @Query

--- src/main/java/com/themeeplers/statistics/repository/BGGGameRepository.java
+++ src/main/java/com/themeeplers/statistics/repository/BGGGameRepository.java
@@ -2,6 +2,7 @@ package com.themeeplers.statistics.repository;
 
 import com.themeeplers.statistics.model.db.BGGGame;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +10,9 @@ import org.springframework.stereotype.Repository;
 public interface BGGGameRepository extends PagingAndSortingRepository<BGGGame, Long> {
     BGGGame findByBggId(Long bggId);
 
+    @Query("SELECT max(a.weight)) FROM GameEntry a")
     BGGGame findTopOrderByWeightDesc();
 
+    @Query("SELECT max(a.rating)) FROM GameEntry a")
     BGGGame findTopOrderByRatingDesc();
 }
