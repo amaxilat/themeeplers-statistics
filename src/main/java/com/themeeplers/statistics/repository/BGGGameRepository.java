c471197a4ac0096258dd989080d87bb00c1f0b29
amaxilat <d.amaxilatis@sparkworks.net>
2018-03-30 01:01:11 +0300

    add sorting to repository

--- src/main/java/com/themeeplers/statistics/repository/BGGGameRepository.java
+++ src/main/java/com/themeeplers/statistics/repository/BGGGameRepository.java
@@ -2,10 +2,11 @@ package com.themeeplers.statistics.repository;
 
 import com.themeeplers.statistics.model.db.BGGGame;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BGGGameRepository extends JpaRepository<BGGGame, Long> {
+public interface BGGGameRepository extends PagingAndSortingRepository<BGGGame, Long> {
     BGGGame findByBggId(Long bggId);
 
     BGGGame findTopOrderByWeightDesc();
